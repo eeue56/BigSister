e1f8fecfaec85ff60349a678fa5eedf8967729a8
eeue56 <eeue56@bangor.ac.uk>
2012-09-04 21:25:23 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "BigSister",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
 
     install_requires = ['requests',
