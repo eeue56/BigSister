12e33878de7d7d4414e28a2b2432f0f7e7cfec62
eeue56 <eeue56@bangor.ac.uk>
2012-09-13 22:25:21 +0100

    Working on improving decks and cards

--- big/general/games/cards.py
+++ big/general/games/cards.py
@@ -1,46 +1,63 @@
 from random import shuffle
 
 SUITS = { 0 : 'Hearts',
-		  1 : 'Diamonds',
-		  2 : 'Clubs',
-		  3 : 'Spades' }
+          1 : 'Diamonds',
+          2 : 'Clubs',
+          3 : 'Spades' }
 
 CARDS = { k, v for x, str(x) in x in xrange(2, 11)}
 
 CARDS.update({ 
-	11 : 'Jack',
-	12 : 'Queen',
-	13 : 'King',
-	14 : 'Ace'
-	})
+    11 : 'Jack',
+    12 : 'Queen',
+    13 : 'King',
+    14 : 'Ace'
+    })
 
-class Desk(object):
+class NoEnoughCards(Exception):
+    pass
 
-	def __init__(self):
-		self.cards = [Card(value, suit) for value in CARDS
-					 	for suit in SUITS]
-		self.shuffle()
 
-	def shuffle(self):
-		shuffle(self.cards)
+class Deck(object):
 
-	def __iter__(self):
-		return self
+    def __init__(self):
+        self.cards = []
+        self.create()
+        self.shuffle()
 
-	def next(self):
-		for card in self.cards[:]:
-			yield card
-		raise StopIteration
+    def push_to_bottom(self, card):
+        self.cards.append(card)
+
+    def create(self):
+        self.cards = [Card(value, suit) for value in CARDS
+                        for suit in SUITS]
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        for card in self.cards[:]:
+            yield card
+        raise StopIteration
+
+    def draw_cards(self, amount):
+        if len(self.cards) < amount:
+            raise NotEnoughCards
+        return [self.cards.pop() for x in xrange(amount)]
 
 
 class Card(object):
 
-	def __init__(self, value, suit):
-		self.value = value
-		self.suit = suit
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
 
-	def __eq__(self, other):
-		return self.value == other.value and self.suit == other.suit
+    def __eq__(self, other):
+        return self.value == other.value and self.suit == other.suit
 
-	def __str__(self):
-		return '{} of {}'.format(CARDS[self.value], SUITS[self.suit])
+    def __str__(self):
+        return '{} of {}'.format(CARDS[self.value], SUITS[self.suit])
+
